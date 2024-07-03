e3791e9d23524c88dce28e4ec62f932c8cc48190
chusniadzurriati.1313@gmail.com <chusniadzurriati.1313@gmail.com>
2024-07-03 17:48:11 +0700

    feat: navbar

---
+++ src/app/not-found.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+const NotFound = () => {
+  return (
+  <div>
+    <h2>NotFound</h2>
+    <p>Sorry Langka COK UDU NENG KENE</p>
+    <Link href="/">Retirn Home</Link>
+  </div>
+  )
+}
+
+export default NotFound
